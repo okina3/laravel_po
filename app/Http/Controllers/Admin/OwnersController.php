72ab4adfe01bc7c5051e802768fa53c336b7d3e4
okina3 <takosos@icloud.com>
2023-02-09 13:50:57 +0900

    実際に、バリデーションをかけ、DBに登録できるようにする。

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Owner;   //Eloquent  エロクワント
+// use Illuminate\Support\Facades\DB;  クエリビルダ
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
 
 
 class OwnersController extends Controller
@@ -25,7 +28,6 @@ class OwnersController extends Controller
     {
         $owners = Owner::select('name', 'email', 'created_at')->get();
         return view('admin.owners.index', compact('owners'));
-
     }
 
     /**
@@ -46,7 +48,19 @@ class OwnersController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:' . Owner::class],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        Owner::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return redirect()->route('admin.owners.index');
     }
 
     /**

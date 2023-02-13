507a597e7fd3da5fd09b69dcaa6423b8dcaa0fe3
okina3 <takosos@icloud.com>
2023-02-13 15:07:57 +0900

    オーナーのガードの設定の修正

--- routes/owner.php
+++ routes/owner.php
@@ -34,7 +34,7 @@ Route::get('/', function () {
 //ダッシュボードのルーティング
 Route::get('/dashboard', function () {
    return view('owner.dashboard');
-})->middleware(['auth::owners', 'verified'])->name('dashboard');
+})->middleware(['auth:owners', 'verified'])->name('dashboard');
 
 // Route::middleware('auth')->group(function () {
 //    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
